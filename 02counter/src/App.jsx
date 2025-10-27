ac029c97dbf08fa0c12f515cd3716ecc0ecb86e6
ABHISHEK GUPTA <abhishekgupta1864@gmail.com>
2025-10-27 16:00:17 +0530

    useState

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,35 @@
+import {useState} from 'react'
+
+import './App.css'
+
+function App() {
+  let [counter,setCounter] = useState(0);
+
+  const addValue = () =>{
+    if(counter==20){
+      return;
+    }else{
+      setCounter(counter+1);
+    } 
+  };
+
+  const removeValue = () =>{
+    if (counter == 0) {
+      return;
+    }else{
+     setCounter(counter-1)
+    }
+  };
+  return (
+    <>
+      <h3>Show Counter Value from 0 to 20 </h3>
+      <h2>Counter Value:{counter}</h2>
+       <button onClick={addValue}>Add value</button>
+       <br />
+       <br />
+       <button onClick={removeValue}>Remove value</button>
+    </>
+  )
+}
+
+export default App
