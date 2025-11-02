13f2c2578abc3c2453a8982b8f8d56378d3ecfe8
ABHISHEK GUPTA <abhishekgupta1864@gmail.com>
2025-11-03 02:53:17 +0530

    redux

---
+++ 11reduxToolkit/src/components/AddTodo.jsx
@@ -0,0 +1,39 @@
+import {useState} from "react"
+
+import {useDispatch} from 'react-redux'
+
+import {addTodo} from "../features/todo/todoSlice"
+
+function AddTodo(){
+  const [input,setInput] = useState('')
+   const dispatch = useDispatch()
+
+   const addTodoHandler = (e) =>{
+     e.preventDefault()
+     dispatch(addTodo(input))
+     setInput('')
+   }
+
+
+ return(
+  <>
+  <form onSubmit={addTodoHandler}>
+     <input type="text" 
+     placeholder="Enter a Todo..."
+     value={input}
+     onChange={(e)=>setInput(e.target.value)}/>
+
+    <br /><br />
+    <button type="submit">
+      Add Todo
+    </button>
+    
+
+
+   </form> 
+   <br />
+  </>
+ )
+}
+
+export default AddTodo
---
+++ 11reduxToolkit/src/components/Todos.jsx
@@ -0,0 +1,26 @@
+import {useSelector,useDispatch} from 'react-redux'
+import {removeTodo} from "../features/todo/todoSlice"
+
+
+function Todos(){
+  const todos = useSelector(state=>state.todos )
+  const dispatch = useDispatch()
+
+  return(
+    <>
+      <div>Todos</div>
+      {todos.map((todo)=>(
+        <li key={todo.id}>
+          {todo.text}
+          
+          <button onClick={()=>dispatch(removeTodo(todo.id))}>X</button>
+        </li>
+        
+        
+      ))}
+      <br />
+    </>
+  )
+}
+
+export default Todos
